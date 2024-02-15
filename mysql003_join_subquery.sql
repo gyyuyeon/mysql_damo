85ffffb829bffa1d3de3e4caff1ed98769f669bb
gyyuyeon <gyyuyeon@gmail.com>
2024-02-15 12:34:21 +0900

    new

--- mysql003_join_subquery.sql
+++ mysql003_join_subquery.sql
@@ -197,3 +197,280 @@ select e.first_name, d.department_id, d.department_name   -- 일치하는 컬럼
 from employees e inner join departments d
 -- on e.department_id = d.department_id; -- 106
 using(department_id); -- 위에거랑 같지만 권장하진 않음
+
+/*-----------------------------------------------------------------------------------------
+서브쿼리(subquery)
+ 하나의 SQL문안에 포함되어 있는 또 다른 SQL문을 말한다.
+ 서브쿼리는 알려지지 않은 기준을 이용한 검색을 위해 사용한다.
+ 서브쿼리는 메인쿼리가 서브쿼리를 포함하는 종속적인 관계이다.
+ 서브쿼리는 메인쿼리의 컬럼을 모두 사용할 수 있지만 메인쿼리는 서브쿼리의 컬럼을 사용할 수 없다. 
+ 질의 결과에 서브쿼리 컬럼을 표시해야 한다면 조인방식으로 
+    변환하거나 함수, 스칼라 서브쿼리(scarar subquery)등을 사용해야 한다. 
+ 조인은 집합간의 곱(Product)의 관계이다. 
+ 
+외부 쿼리 (메인쿼리)
+ :일반 쿼리를 의미합니다.
+스칼라 서브쿼리
+ :SELECT 절에 쿼리가 사용되는 경우로, 함수처럼 레코드당 정확히 하나의 값만을 반환하는 서브쿼리입니다.
+인라인 뷰
+ :FROM 절에 사용되는 쿼리로, 원하는 데이터를 조회하여 가상의 집합을 만들어 조인을 수행하거나 가상의 집합을 다시 조회할 때 사용합니다.
+
+
+
+서브쿼리를 사용할 때 다음 사항에 주의
+  서브쿼리를 괄호로 감싸서 사용한다. 
+  서브쿼리는 단일 행(Single Row) 또는 복수 행(Multiple Row) 비교 연산자와 함께 사용 가능하다. 
+  단일 행 비교 연산자는 서브쿼리의 결과가 반드시 1건 이하이어야 하고 복수 행 비교 연산자는 서브쿼리의 결과 건수와 상관 없다. 
+  서브쿼리에서는 ORDER BY를 사용하지 못한다. 
+  ORDER BY절은 SELECT절에서 오직 한 개만 올 수 있기 때문에 ORDER BY절은 메인쿼리의 마지막 문장에 위치해야 한다.
+  
+
+서브 쿼리 사용가능한 위치
+SELECT, FROM, WHERE, HAVING,ORDER BY 
+INSERT문의 VALUES,
+UPDATE문의 SET, 
+CREATE문
+
+서브쿼리의 종류는 동작하는 방식이나 반환되는 데이터의 형태에 따라 분류할 수 있다.
+1 동작하는 방식에 따른 서브쿼리 분류
+  Un-Correlated(비연관) : 서브쿼리가 메인쿼리 컬럼을 가지고 있지 않는 형태의 서브쿼리이다.
+          메인쿼리에 값(서브쿼리가 실행된 결과)를 제공하기 위한 목적으로  주로 사용한다.
+  Correlated(연관) : 서브쿼리가 메인쿼리 칼럼을 가지고 있는 형태의 서브쿼리이다.
+          일반적으로 메인쿼리가 먼저 수행되어 읽혀진 데이터를 서브쿼리에서 조건이 맞는지 확인
+	  하고자 할 때 주로 사용된다.  (EXISTS서브쿼리는 항상 연관 서브쿼리로 사용된다. 조건을 만족하는 1건만 찾으면
+	  추가 검색을 하지 않는다.)
+2 반환되는 데이터의 형태에 따른 서브쿼리 종류
+  Single Row(단일행 서브쿼리) : 서브쿼리의 실행결과가 항상 1건 이하인 서브쿼리를 의미한다. 
+          단일행 서브쿼리는 단일 행 비교 연산자와 함께 사용된다.
+	  단일 행 비교 연산자는 =, <, <=, >, >=, <>이 있다.
+  Multi Row(다중행 서브쿼리) : 서브쿼리의 실행 결과가 여러 건인 서브쿼리를 의미한다. 
+          다중 행 서브쿼리는 다중 행 비교 연산자와 함께 사용된다. 
+	  다중 행 비교 연산자에는 in, all, any, some, exists가 있다.
+	      in : 메인쿼리의 비교조건('='연산자로 비교할 경우)이 서브쿼리의 결과 중에서
+               하나라도 일치하면 참이다.
+           any,some : 메인 쿼리의 비교 조건이 서브 쿼리의 검색 결과와 하나 이상이 일치하면
+                참이다.
+           all : 메인 쿼리의 비교 조건이 서브 쿼리의 검색 결과와 모든 값이 일치하면 참이다.
+           exists : 메인 쿼리의 비교 조건이 서브 쿼리의 결과 중에서 만족하는 값이 하나라도
+               존재하면 참이다.
+  Multi Column(다중칼럼 서브쿼리) : 서브쿼리의 실행 결과로 여러 컬럼을 반환한다.
+          메인쿼리의 조건절에 여러 컬럼을 동시에 비교할 수 있다. 
+	  서브쿼리와 메인쿼리에서 비교하고자 하는 컬럼 갯수와 컬럼의 위치가 동일해야 한다.
+--------------------------------------------------------------------------------- */ 
+-- 90번 부서에 근ㄴ무하는 Lex의 부서명을 출력하시오
+select department_name
+from departments
+where department_id = 90;
+
+-- Lex가 근무하는 부서명을 출력하시오
+select department_id 
+from employees
+where first_name = 'Lex';
+
+select department_name
+from departments
+where department_id = 90;
+
+select d.department_name
+from employees e inner join departments d
+on e.department_id = d.department_id
+where e.first_name = 'Lex';
+
+select department_name
+from departments
+where department_id = (select department_id 
+						from employees
+						where first_name = 'Lex');
+            
+-- 'Lex'와 동일한 업무(job_id)를 가진 사원의 이름(first_name), 
+ -- 업무명(job_title), 입사일(hire_date)을 출력하시오.
+ 
+ select e.first_name, j.job_title, e.hire_date
+ from employees e inner join jobs j
+on e.job_id = j.job_id
+where j.job_title = 'Administration Vice President';
+
+select e.first_name, j.job_title, e.hire_date
+from employees e inner join jobs j
+on e.job_id = j.job_id
+where e.job_id = (select job_id 
+						from employees
+						where first_name = 'Lex');
+
+-- 'IT'에 근무하는 사원이름(first_name), 부서번호을 출력하시오.
+
+
+select first_name, department_id
+from employees 
+where department_id = (select department_id 
+						from departments
+						where department_name ='IT');
+
+
+-- 'Bruce'보다 급여를 많이 받은 사원이름(first_name), 부서명, 급여를 출력하시오.
+select e.first_name, d.department_name, e.salary
+from employees e inner join departments d
+on e.department_id = d.department_id
+where salary > (select salary
+				from employees
+                where first_name ='Bruce')
+order by salary;
+
+-- Steven와 같은 부서에서 근무하는 사원의 이름, 급여, 입사일을 출력하시오.(in)  -- 1개의 행보다 더많은 정보가 리턴(실행안됨)
+-- 오류 예제
+select first_name, salary, hire_date
+from employees
+where department_id = ( select department_id
+						from employees
+                        where first_name = 'Steven');
+-- 수정 예제
+select first_name, salary, hire_date
+from employees
+where department_id in (50,90);
+
+-- 수정 예제 최종
+select first_name, salary, hire_date
+from employees
+where department_id in ( select department_id
+						from employees
+                        where first_name = 'Steven');
+
+-- 부서별로 가장 급여를 많이 받는 사원이름, 부서번호, 급여를 출력하시오.(in)
+select department_id, max(salary)
+from employees
+group by department_id;
+
+select first_name, department_id, salary
+from employees
+where (department_id, salary) in ( select department_id, max(salary) -- where 자리에선 컬럼이 와야되지 그룹함수가 오면 안된다 max(salary)
+										from employees
+										group by department_id)
+ORDER BY department_id asc;
+
+
+-- 30소속된 사원들 중에서 급여를 가장 받은 사원보다 더 많은 급여를 받는
+-- 사원이름, 급여, 입사일을 출력하시오. (ALL)
+-- (서브쿼리에서 max()함수를 사용하지 않는다);
+
+select salary
+from employees
+where department_id = 30;
+
+
+-- 모든것에 관하여 만족
+select first_name, salary, hire_date
+from employees
+where salary > all(select salary
+				   from employees
+				   where department_id = 30);
+
+-- 30소속된 사원들이 받은 급여보다  높은 급여를 받는 
+-- 사원이름, 급여, 입사일을 출력하시오. (ANY)
+-- (서브쿼리에서 min()함수를 사용하지 않는다);
+
+select first_name, salary, hire_date
+from employees
+where salary > any(select salary
+				   from employees
+				   where department_id = 30);
+                   
+-- 사원이 있는 부서만 출력하시오.
+SELECT department_id, department_name
+from departments;  -- 27 row
+
+select distinct department_id  -- 중복제거 한번만 select자리에서
+from employees;  -- 12개
+
+SELECT department_id, department_name -- 11개
+from departments
+where department_id in (select distinct department_id
+						from employees);
+
+
+/*-----------------------------------------------------
+ 상관관계 서브쿼리
+ : 서브쿼리에서 메인쿼리의 컬럼을 참조한다.(메인쿼리를 먼저수행한다.)
+   서브쿼리는 메인쿼리 각각의 행에 대해서 순서적으로 한번씩 실행한다.
+ <아래 쿼리 처리순서>
+ 1st : 바깥쪽 쿼리의 첫째 row에 대하여 
+ 2nd : 안쪽 쿼리에서 자신의 속해있는 부서의 MAX salary과
+       비교하여 true 이면 바깥의 컬럼값을 반환하고 , 
+       false 이면 값을 버린다. 
+ 3rd : 바깥쪽 쿼리의 두 번째 row에 대하여 마찬가지로 실행하며, 
+       이렇게 바깥쪽 쿼리의 마지막 row까지 실행한다. 
+	   
+https://www.w3resource.com/sql/subqueries/correlated-subqueries-using-aliases.php	   
+----------------------------------------------------*/
+
+-- 부서별 최고 급여를 받는 사원을 출력하시오.
+
+select department_id, max(salary)
+from employees
+group by department_id;
+
+select department_id, salary, first_name, hire_date
+from employees
+where (department_id, salary) in ( select department_id, max(salary) -- where 자리에선 컬럼이 와야되지 그룹함수가 오면 안된다 max(salary)
+										from employees
+										group by department_id);
+
+select department_id, salary, first_name, hire_date
+from employees e
+where salary = (select max(salary)
+				from employees
+                where department_id = e.department_id)
+order by department_id;
+
+select department_id , salary
+from employees;
+
+-- 사원이 없는 부서만 출력하시오
+
+
+select distinct department_id
+from employees;  -- 12row
+
+select department_id
+from departments;  -- 27row
+
+select department_id, department_name
+from departments 
+where department_id not in (select distinct department_id
+							from employees
+                            where department_id is not null);  -- 16
+
+select department_id, department_name
+from departments d
+WHERE not exists (select 1 
+			from employees e
+            where e.department_id = d.department_id);  -- 16
+        
+-- ???/ 
+ SELECT 
+    DEPARTMENT_ID, DEPARTMENT_NAME
+FROM
+    departments d
+WHERE
+    NOT EXISTS( SELECT 
+            1
+        FROM
+            employees e
+        WHERE
+            e.DEPARTMENT_ID = d.DEPARTMENT_ID);           
+            
+            
+-- 관리자가 있는 사원의 정보만 출력하시오
+select e.employee_id, e.first_name, e.department_id
+from employees e
+where  exists ( select 1
+				from employees m
+				where m.employee_id = e.manager_id);
+                
+-- 관리자가 없는 사원의 정보만 출력하시오
+select e.employee_id, e.first_name, e.department_id
+from employees e
+where not exists ( select 1
+				from employees m
+				where m.employee_id = e.manager_id);
+                
+                
